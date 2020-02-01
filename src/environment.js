9005f02db8b6e3f031f1b4b8624c7e416921851e
Cristian Conache <cconache@gmail.com>
2020-02-02 00:46:00 +0200

    Environment variables test

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,10 @@
+function isDevEnvironment() {
+  return process.env.NODE_ENV === "development";
+}
+
 export const ENV = {
-  base_url: 'http://localhost:3000',
+  base_url: isDevEnvironment() ? "http://localhost:3000" : "http://qa-application.s3-website.eu-central-1.amazonaws.com",
   client_env: 'development',
 };
 
-export const __BASE_API_URL__ = 'http://localhost:8085';
+export const __BASE_API_URL__ = isDevEnvironment() ? "http://18.195.101.10:8080" : "http://18.195.101.10:8080";
