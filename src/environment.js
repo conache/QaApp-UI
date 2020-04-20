c6e223828c782ed16553a6dd4a4d0b9d6204aeb4
Condition17 <c.conache@gmail.com>
2020-04-20 17:46:22 +0300

    Upadted socket server url

--- src/environment.js
+++ src/environment.js
@@ -3,9 +3,15 @@ function isDevEnvironment() {
 }
 
 export const ENV = {
-  base_url: isDevEnvironment() ? "http://localhost:3000" : "http://qa-application.s3-website.eu-central-1.amazonaws.com",
-  client_env: 'development',
+  base_url: isDevEnvironment()
+    ? "http://localhost:3000"
+    : "http://qa-application.s3-website.eu-central-1.amazonaws.com",
+  client_env: "development",
 };
 
-export const __BASE_API_URL__ = isDevEnvironment() ? "http://localhost:8085" : "http://18.195.101.10:8080";
-export const _BASE_SOCKET_SERVER_URL_ = isDevEnvironment() ? "http://3.134.101.74:8080" : "http://3.134.101.74:8080";
+export const __BASE_API_URL__ = isDevEnvironment()
+  ? "http://localhost:8085"
+  : "http://18.195.101.10:8080";
+export const _BASE_SOCKET_SERVER_URL_ = isDevEnvironment()
+  ? "http://3.16.109.51:8080"
+  : "http://3.16.109.51:8080";
